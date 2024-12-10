03e76c0f0b0cfa2c10b916c0d9d9c46f7441c1d6
Isaac Destura <isaac.destura@sita.aero>
2024-12-10 14:51:04 +0100

    Day 10 part 2

--- AoC2024/Day10.cs
+++ AoC2024/Day10.cs
@@ -34,7 +34,7 @@ public class Day10
             for (var col = 0; col < _grid[0].Length ; col++)
             {
                 if (_grid[row][col] == '0')
-                    total += Part1Score(row, col);
+                    total += Part2Score(row, col);
             }
         }
 
@@ -87,5 +87,50 @@ public class Day10
         return reachablePeaks.Count;
     }
     
+    private int Part2Score(int row, int col)
+    {
+        var toVisit = new Stack<Tuple<int, int>>();
+        toVisit.Push(new Tuple<int, int>(row, col));
+        bool[,] visited = new bool[_grid.Length, _grid[0].Length];
+        var total = 0;
+
+        while (toVisit.Count > 0)
+        {
+            var current = toVisit.Pop();
+
+            if (_grid[current.Item1][current.Item2] == 'X')
+                continue;
+            
+            // look at adjacent grid squares
+            // if current is 8, look around and check for 9's and increment the total
+            // otherwise, look around for 1 higher than current's value and add to stack
+            if (_grid[current.Item1][current.Item2] == '8')
+            {
+                for (var i = 0; i < 4; i++)
+                {
+                    if (IsCoordinateValid(current.Item1 + _dy[i], current.Item2 + _dx[i]) &&
+                        _grid[current.Item1 + _dy[i]][current.Item2 + _dx[i]] == '9')
+                        total++;
+
+                }
+            }
+            else
+            {
+                int currHeight = _grid[current.Item1][current.Item2] - '0';
+                
+                for (var i = 0; i < 4; i++)
+                {
+                    if (IsCoordinateValid(current.Item1  + _dy[i], current.Item2 + _dx[i]) &&
+                        _grid[current.Item1 + _dy[i]][current.Item2 + _dx[i]] == (char)(currHeight + 1 + '0'))
+                        toVisit.Push(new Tuple<int, int>(current.Item1 + _dy[i], current.Item2 + _dx[i]));
+                }
+                
+            }
+            // mark of the location as visited
+            visited[current.Item1,current.Item2] = true;
+        }
+
+        return total;
+    }
     private bool IsCoordinateValid(int row, int col) => row >= 0 && row < _grid.Length && col >= 0 && col < _grid[0].Length;
 }
--- AoC2024/Program.cs
+++ AoC2024/Program.cs
@@ -1,7 +1,8 @@
 ﻿using AoC2024;
 
 var challenge = new Day10(Lines("day10.txt").ToArray());
-Console.WriteLine(challenge.Part1());
+// Console.WriteLine(challenge.Part1());
+Console.WriteLine(challenge.Part2());
 return;
 
 IEnumerable<string> Lines(string path)
